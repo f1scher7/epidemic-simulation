654e0775d8234ea7e59c40a2cb47d20303a0c749
f1scher7 <maksym.szylo@gmail.com>
2024-07-25 15:57:10 +0200

    Added deleteAllBySimulationId and findAllBySimulationId funcs

--- src/main/java/com/treative/epidemicsimulation/repository/DailySimulationDataRepository.java
+++ src/main/java/com/treative/epidemicsimulation/repository/DailySimulationDataRepository.java
@@ -2,11 +2,24 @@ package com.treative.epidemicsimulation.repository;
 
 import com.treative.epidemicsimulation.entity.DailySimulationData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface DailySimulationDataRepository extends JpaRepository<DailySimulationData, Long> {
 
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM DailySimulationData simData WHERE simData.simulation.id = :simualtionId")
+    void deleteAllBySimulationId(@Param("simulationId") Long simulationId);
 
+    @Transactional
+    @Query("SELECT simData FROM DailySimulationData simData WHERE simData.simulation.id = :simulationId")
+    List<DailySimulationData> findAllBySimulationId(@Param("simulationId") Long simulationId);
 
 }
