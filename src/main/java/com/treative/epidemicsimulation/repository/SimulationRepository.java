30105fffdc4bf44a46d49f546af85710929980c7
f1scher7 <maksym.szylo@gmail.com>
2024-07-25 15:57:39 +0200

    Added findByName func

--- src/main/java/com/treative/epidemicsimulation/repository/SimulationRepository.java
+++ src/main/java/com/treative/epidemicsimulation/repository/SimulationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SimulationRepository extends JpaRepository<Simulation, Long> {
 
-
+    Simulation findByName(String name);
 
 }
