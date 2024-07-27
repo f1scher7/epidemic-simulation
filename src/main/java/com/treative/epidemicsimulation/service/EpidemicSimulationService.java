9bd5398ead5388b0a56310b1c5af58ced84fd21f
f1scher7 <maksym.szylo@gmail.com>
2024-07-27 23:41:15 +0200

    Added advance logic for epidemic simulation

--- src/main/java/com/treative/epidemicsimulation/service/EpidemicSimulationService.java
+++ src/main/java/com/treative/epidemicsimulation/service/EpidemicSimulationService.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static com.treative.epidemicsimulation.utils.EpidemicSimulationUtil.*;
+
 @Service
 public class EpidemicSimulationService {
 
@@ -28,40 +30,63 @@ public class EpidemicSimulationService {
         List<DailySimulationData> dailySimulationDataList = new ArrayList<>();
 
         int totalPopulation = simulation.getPopulation();
-        int infected = simulation.getInitialInfected();
-        int healthy = totalPopulation - infected;
-        int dead = 0;
+        int initialInfected = simulation.getInitialInfected();
+        int healthy = totalPopulation - initialInfected;
         int recovered = 0;
-        int daysToDeath = simulation.getDaysToDeath();
-        int daysToRecovery = simulation.getDaysToRecovery();
+        int totalHealthy = healthy + recovered;
+        int dead = 0;
+        double restrictionLevel = 0.0;
+        double peopleMood;
         double infectionRate = simulation.getInfectionRate();
         double mortalityRate = simulation.getMortalityRate();
-
-        List<Integer> infectedDaysList = new ArrayList<>(Collections.nCopies(infected, 0));
+        int infectionsDaysDelay = simulation.getInfectionDaysDelay();
+        int daysToDeath = simulation.getDaysToDeath();
+        int daysToRecovery = simulation.getDaysToRecovery();
+        List<Integer> infectedDaysList = new ArrayList<>(Collections.nCopies(initialInfected, 0));
 
         for (int day = 1; day <= simulation.getSimulationDays(); day++) {
             int[] deadRecoveryCountArray = {0, 0};
 
             infectedDaysList = updateInfectedDays(infectedDaysList, daysToDeath, daysToRecovery, deadRecoveryCountArray, mortalityRate);
 
+            int infected = infectedDaysList.size();
             dead += deadRecoveryCountArray[0];
             recovered += deadRecoveryCountArray[1];
 
-            int newInfections = (int) (infectedDaysList.size() * infectionRate);
-            newInfections = Math.min(newInfections, healthy);
+            if (day % 10 == 0) {
+                restrictionLevel = calculateRestrictionLevel(restrictionLevel, totalPopulation, totalHealthy, infected, dead);
+            }
+
+            if (infected == 0) {
+                restrictionLevel = 0.0;
+            }
+
+            peopleMood = calculatePeopleMood(totalHealthy, infected, dead, totalPopulation, restrictionLevel);
+            infectionRate = calculateUpdatedInfectionRate(infectionRate, restrictionLevel, infected, totalPopulation);
+
+            if (infected == 0) {
+                infectionRate = 0.0;
+            }
+
+            System.out.println("restrictiveLevel: " + restrictionLevel + "; peopleMood: " + peopleMood + "; infectionRate: " + infectionRate);
+
+
+            int newInfections = calculateNewInfections(infectedDaysList, healthy, infectionRate, infectionsDaysDelay);
 
             healthy -= newInfections;
+            totalHealthy = healthy + recovered;
 
             for (int i = 0; i < newInfections; i++) {
                 infectedDaysList.add(0);
             }
 
-            dailySimulationDataList.add(new DailySimulationData(simulation, infectedDaysList.size(), healthy, dead, recovered, day));
+            dailySimulationDataList.add(new DailySimulationData(simulation, infectedDaysList.size(), healthy, dead, recovered, restrictionLevel, peopleMood, day));
         }
+
         return this.dailySimulationDataService.saveAllDailySimulationData(dailySimulationDataList);
     }
 
-    public List<Integer> updateInfectedDays(List<Integer> infectedDaysList, int daysToDeath, int daysToRecovery, int[] deadRecoveredCountArray, double mortalityRate) {
+    private List<Integer> updateInfectedDays(List<Integer> infectedDaysList, int daysToDeath, int daysToRecovery, int[] deadRecoveredCountArray, double mortalityRate) {
         return infectedDaysList.stream()
                 .map(daysInfected -> {
                     if (daysInfected + 1 == daysToDeath || Math.random() < mortalityRate) {
