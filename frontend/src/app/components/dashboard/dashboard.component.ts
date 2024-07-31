ef8d505f6bde293989ee4607e5605dd96db4d47a
f1scher7 <maksym.szylo@gmail.com>
2024-07-31 10:38:55 +0200

    Added funcs for SimulationCreateModal

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {SimulationModel} from "../../models/simulation.model";
 import {SimulationService} from "../../services/simulation.service";
-import {DecimalPipe, NgForOf, PercentPipe} from "@angular/common";
+import {DecimalPipe, NgClass, NgForOf, PercentPipe} from "@angular/common";
+import {SimulationCreateModalComponent} from "../simulation-create-modal/simulation-create-modal.component";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-dashboard',
@@ -9,7 +11,9 @@ import {DecimalPipe, NgForOf, PercentPipe} from "@angular/common";
     imports: [
         PercentPipe,
         DecimalPipe,
-        NgForOf
+        NgForOf,
+        SimulationCreateModalComponent,
+        NgClass
     ],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.css'
@@ -17,23 +21,64 @@ import {DecimalPipe, NgForOf, PercentPipe} from "@angular/common";
 export class DashboardComponent implements OnInit {
 
     simulations: SimulationModel[] =  [];
+    isSimulationCreateModalVisible = false;
+    errors: any = {};
 
-    constructor(private simulationService: SimulationService) {}
+    constructor(private router: Router, private simulationService: SimulationService) {}
 
-    ngOnInit(): void {
+    ngOnInit() {
         this.loadAllSimulations();
     }
 
     loadAllSimulations() {
-        this.simulationService.getAllSimulations().subscribe(
-            (data: SimulationModel[]) => {
+        this.simulationService.getAllSimulations().subscribe({
+            next: (data: SimulationModel[]) => {
                 this.simulations = data;
                 console.log(data);
             },
-            error => {
-                console.log(error);
+            error: (err) => {
+                console.log(err);
             }
-        )
+        })
+    }
+
+    createSimulation(simulation: SimulationModel) {
+        this.simulationService.createSimulation(simulation).subscribe({
+            next: (simulation) => {
+                window.location.reload();
+                this.errors = {};
+            },
+            error: (err) => {
+                console.log(err);
+                this.errors = { createSimulation: err };
+            }
+        })
+    }
+
+    viewSimulationDetails(simulation: SimulationModel) {
+        this.router.navigate(['/simulation', simulation.id], { state: { simulation } });
+    }
+
+    deleteSimulation(simulation: SimulationModel) {
+        if (confirm(`Are you sure you want to delete simulation ${simulation.name}?`)) {
+            this.simulationService.deleteSimulation(simulation).subscribe({
+                next: () => {
+                    window.location.reload();
+                },
+                error: err => {
+                    console.log('Error deleting simulation', err);
+                }
+            })
+        }
+    }
+
+    showSimulationCreateModal() {
+        this.isSimulationCreateModalVisible = true;
+    }
+
+    closeSimulationCreateModal() {
+        this.errors = {};
+        this.isSimulationCreateModalVisible = false;
     }
 
 }
