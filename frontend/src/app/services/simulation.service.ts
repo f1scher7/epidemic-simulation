511df21745be5845ee37d44a67f60916a302aa92
f1scher7 <maksym.szylo@gmail.com>
2024-07-31 10:40:05 +0200

    Added getSimulationById, createSimulation, deleteSimulation and handleError funcs

--- frontend/src/app/services/simulation.service.ts
+++ frontend/src/app/services/simulation.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {map, Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, map, Observable, throwError} from "rxjs";
 import {SimulationModel} from "../models/simulation.model";
 
 @Injectable({
@@ -29,6 +29,47 @@ export class SimulationService {
                 }));
             })
         );
+    }
+
+    getSimulationById(id: number): Observable<SimulationModel> {
+        return this.http.get<SimulationModel>(`${this.baseUrl}/get-by-id/${id}`).pipe(
+            map(simulation => ({
+                id: simulation.id,
+                name: simulation.name,
+                population: simulation.population,
+                initialInfected: simulation.initialInfected,
+                infectionRate: simulation.infectionRate,
+                mortalityRate: simulation.mortalityRate,
+                infectionDaysDelay: simulation.infectionDaysDelay,
+                daysToRecovery: simulation.daysToRecovery,
+                daysToDeath: simulation.daysToDeath,
+                simulationDays: simulation.simulationDays
+            })),
+            catchError(err => {
+                console.error('Error fetching simulation:', err);
+                return throwError(() => new Error(`Failed to fetch simulation details: ${err}`));
+            })
+        );
+    }
+
+    createSimulation(simulation: SimulationModel): Observable<SimulationModel> {
+        return this.http.post<SimulationModel>(`${this.baseUrl}/create`, simulation).pipe(catchError(this.handleError))
+    }
+
+    deleteSimulation(simulation: SimulationModel) {
+        return this.http.delete(`${this.baseUrl}/delete-by-id/${simulation.id}`);
+    }
+
+    private handleError(error: HttpErrorResponse): Observable<never> {
+        let errorMessage = 'An unexpected error occurred';
+
+        if (error.error) {
+            errorMessage = error.error[Object.keys(error.error)[0]];
+        } else if (error.message) {
+            errorMessage = error.message;
+        }
+
+        return throwError(() => new Error(errorMessage));
     }
 
 }
