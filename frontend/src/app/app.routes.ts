ea9824ae6b872053a01ac7b07dd9f926bef919cc
f1scher7 <maksym.szylo@gmail.com>
2024-07-31 10:35:39 +0200

    Added SimulationDetailsComponent to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import {SimulationDetailsComponent} from "./components/simulation-details/simulation-details.component";
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
+    { path: 'simulation/:id', component: SimulationDetailsComponent },
     { path: '**', redirectTo: '' }
 ];
